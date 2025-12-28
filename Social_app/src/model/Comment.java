779e381673f7b049abba84d4ecc63e714923fad8
ImNourAH <nourayhag@gmail.com>
2025-12-28 22:36:54 +0200

    added id to post with appropriate methods, added comment class, started PostDAO

---
+++ Social_app/src/model/Comment.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Comment {
+    private static int commentCounter = 0;
+    private String content;
+    private User user;
+    private int postID;
+    private int commentID;
+    private LocalDateTime commentTime;
+
+    public Comment(String content, User user, int postID) {
+        this.content = content;
+        this.user = user;
+        this.postID = postID;
+        this.commentID = commentCounter++;
+        this.commentTime = LocalDateTime.now();
+    }
+
+    public String getContent() {return content;}
+    public User getUser() {return user;}
+    public int getPostID() {return postID;}
+    public int getCommentID() {return commentID;}
+    public LocalDateTime getCommentTime() {return commentTime;}
+    public int getCommentCounter() {return commentCounter;}
+
+    public void setContent(String content) {this.content = content;}
+    public void setUser(User user) {this.user = user;}
+    public void setPostID(int postID) {this.postID = postID;}
+    public void setCommentTime(){this.commentTime = LocalDateTime.now();}
+    public void setCommentID() {this.commentID = commentCounter++;}
+
+    public void deleteComment(){}
+}
--- Social_app/src/model/Post.java
+++ Social_app/src/model/Post.java
@@ -1,27 +1,32 @@
 package model;
 
 import javax.xml.stream.events.Comment;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class Post {
+    private static int postCounter = 0;
     // attributes
     private String text;
     private String imagePath;
     private int likes;
     private User user;
-    private final Date postCreationDate;
+    private LocalDate postCreationDate;
     private String postCategory;
     private ArrayList<Comment> comments;
+    private int postID;
 
     // Constructor
+    public Post() {}
     public Post(String text, String imagePath, int likes, User user, String postCategory) {
         this.text = text;
         this.imagePath = imagePath;
         this.likes = likes;
         this.user = user;
         this.postCategory = postCategory;
-        this.postCreationDate = new Date();
+        this.postCreationDate = LocalDate.now();
+        this.postID = postCounter++;
     }
 
     // Getters
@@ -29,8 +34,10 @@ public class Post {
     public String getImagePath() {return imagePath;}
     public int getLikes() {return likes;}
     public User getUser() {return user;}
-    public Date getPostCreationDate() {return postCreationDate;}
+    public LocalDate getPostCreationDate() {return postCreationDate;}
     public String getPostCategory() {return postCategory;}
+    public int getPostID() {return postID;}
+    public int getPostCounter() {return postCounter;}
 
     // Setters
     public void setText(String text) {this.text = text;}
@@ -38,6 +45,8 @@ public class Post {
     public void setLikes(int likes) {this.likes = likes;}
     public void setUser(User user) {this.user = user;}
     public void setPostCategory(String postCategory) {this.postCategory = postCategory;}
+    public void setPostID() {this.postID = postCounter++;}
+    public void setPostCreationDate(LocalDate date){this.postCreationDate = date;}
 
     // Methods
     public void addComment(String commentContent, User user) {}
